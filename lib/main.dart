306df1248ea6268be0d75db377fe934c84e805e7
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2023-07-08 15:50:32 +0900

    feat: Firebase初期設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:dynamic_color/dynamic_color.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_todo/features/todo/pages/home_page.dart';
+import 'package:riverpod_todo/firebase_options.dart';
 
 import 'common/utils/constants.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     const ProviderScope(
       child: MyApp(),
